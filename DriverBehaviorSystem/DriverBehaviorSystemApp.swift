4bf5170d76fa44eee11fc9290fa6b8b7873761a4
aleynaaley <aleyna.erkulll@gmail.com>
2026-03-31 00:17:15 +0300

    Initial Commit

---
+++ DriverBehaviorSystem/DriverBehaviorSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriverBehaviorSystemApp.swift
+//  DriverBehaviorSystem
+//
+//  Created by Aley Aley on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct DriverBehaviorSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
