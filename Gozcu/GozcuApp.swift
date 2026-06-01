43dfefdf9437f9528d33565df9ef4a8455112198
aleynaaley <aleyna.erkulll@gmail.com>
2026-06-01 15:00:32 +0300

    Initial Commit

---
+++ Gozcu/GozcuApp.swift
@@ -0,0 +1,17 @@
+//
+//  GozcuApp.swift
+//  Gozcu
+//
+//  Created by Aley Aley on 1.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct GozcuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
